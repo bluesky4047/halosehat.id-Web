84c38a7273d3715180c7356acce08a7b57ddda97
Syabadriel <wongtriyan01@gmail.com>
2025-06-04 16:13:04 +0700

    Add files via upload

---
+++ database/migrations/2025_06_04_091106_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('slug')->unique(); // Untuk URL yang rapi dan unik
+            $table->text('content');
+            $table->string('category')->nullable();
+            $table->string('featured_image')->nullable(); // Path gambar
+            $table->timestamps(); // created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
